499b2a06cc6b4f4c8511746f03558b0be4a2dcef
Tomislav <bekavac.tomislav1994@gmail.com>
2021-03-24 12:24:11 +0100

    Prvi rad na Kalkulatoru u VisualBasic

---
+++ Kalkulator/Kalkulator/Pocetna.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Pocetna
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Pocetna))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.VerzijaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.JednostavniKalkulatorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.VerzijaToolStripMenuItem, Me.JednostavniKalkulatorToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(803, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'VerzijaToolStripMenuItem
+        '
+        Me.VerzijaToolStripMenuItem.Name = "VerzijaToolStripMenuItem"
+        Me.VerzijaToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.VerzijaToolStripMenuItem.Text = "Verzija"
+        '
+        'JednostavniKalkulatorToolStripMenuItem
+        '
+        Me.JednostavniKalkulatorToolStripMenuItem.Name = "JednostavniKalkulatorToolStripMenuItem"
+        Me.JednostavniKalkulatorToolStripMenuItem.Size = New System.Drawing.Size(136, 20)
+        Me.JednostavniKalkulatorToolStripMenuItem.Text = "Jednostavni kalkulator"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 27)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(802, 533)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Pocetna
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(803, 559)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Pocetna"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents VerzijaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents JednostavniKalkulatorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Kalkulator/Kalkulator/Pocetna.vb
@@ -0,0 +1,13 @@
+﻿Public Class Pocetna
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub VerzijaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles VerzijaToolStripMenuItem.Click
+        MsgBox("Prva Verzija", MsgBoxStyle.MsgBoxHelp, "Informacije o verziji")
+    End Sub
+
+    Private Sub JednostavniKalkulatorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles JednostavniKalkulatorToolStripMenuItem.Click
+        JednostavniKalkulator.Show()
+    End Sub
+End Class
